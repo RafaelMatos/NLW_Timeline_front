d33b322db31ad40236b016dfa9600e0b07237b79
RafaelMatos <rafael-matos@outlook.com>
2023-05-19 16:06:14 -0300

    making authentication with github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { cookies } from 'next/headers'
 import { Copyright } from '@/components/Copyright'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
   return (
     <main className=" grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -15,7 +18,7 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes"></div>
 
         {/* Sing In */}
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         {/* Hero */}
         <Hero />
 
