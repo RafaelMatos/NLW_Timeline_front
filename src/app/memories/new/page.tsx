3c149d9d46b582a0401ec2c569fea46d7408dad6
RafaelMatos <rafael-matos@outlook.com>
2023-05-29 12:00:14 -0300

    created new memory page

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -24,9 +24,24 @@ export default function NewMemory() {
           <label
             htmlFor="isPublic"
             className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
-          ></label>
+          >
+            <input
+              type="checkbox"
+              name="isPublic"
+              id="isPublic"
+              value="true"
+              className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
+            />
+            Tornar memória pública
+          </label>
         </div>
-        <input type="file" id="media" className="invisible" />
+        <input type="file" id="media" className="invisible h-0 w-0" />
+        <textarea
+          name="content"
+          spellCheck={false}
+          className="w-full flex-1 resize-none border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        />
       </form>
     </div>
   )
